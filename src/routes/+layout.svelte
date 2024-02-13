d1f33664b8704c3c2d7541e51b2154cc5d7a85d7
Julian Hammer <jh@moinjulian.com>
2024-02-13 12:03:13 +0100

    Add Layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Nav from '$lib/components/Nav.svelte';
+	import './app.scss';
+</script>
+
+<svelte:head>
+	<title>Svorm</title>
+</svelte:head>
+
+<Nav></Nav>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		padding: 1rem;
+		max-width: 38rem;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,21 @@
+<h1>Svorm</h1>
+
+<p class="teaser">Create forms and share them with others.</p>
+
+<p>
+	This app allows you to create forms – called svorms –, which are made up out of simple questions
+	and multiple choices. Share the link with others who can then answer the svorm. The results of
+	each svorm can also be seen.
+</p>
+
+<style>
+	p {
+		margin-bottom: 0.75rem;
+	}
+	.teaser {
+		text-align: center;
+		font-style: italic;
+		font-size: 1.25rem;
+		color: var(--primary-color);
+	}
+</style>
